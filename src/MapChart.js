11962b1b83daff2787c9bacbbbf7d949df270910
Jakob <jakob.fellner@tum.de>
2022-01-25 17:06:14 +0100

    markers

--- src/MapChart.js
+++ src/MapChart.js
@@ -13,20 +13,12 @@ import ReactTooltip from "react-tooltip";
 const markers = [
   {
     markerOffset: -15,
-    name: "Buenos Aires",
-    coordinates: [-58.3816, -34.6037]
+    name: "Neue Marker",
+    coordinates: [8,51]
   },
-  { markerOffset: -15, name: "La Paz", coordinates: [-68.1193, -16.4897] },
-  { markerOffset: 25, name: "Brasilia", coordinates: [-47.8825, -15.7942] },
-  { markerOffset: 25, name: "Santiago", coordinates: [-70.6693, -33.4489] },
-  { markerOffset: 25, name: "Bogota", coordinates: [-74.0721, 4.711] },
-  { markerOffset: 25, name: "Quito", coordinates: [-78.4678, -0.1807] },
-  { markerOffset: -15, name: "Georgetown", coordinates: [-58.1551, 6.8013] },
-  { markerOffset: -15, name: "Asuncion", coordinates: [-57.5759, -25.2637] },
-  { markerOffset: 25, name: "Paramaribo", coordinates: [-55.2038, 5.852] },
-  { markerOffset: 25, name: "Montevideo", coordinates: [-56.1645, -34.9011] },
-  { markerOffset: -15, name: "Caracas", coordinates: [-66.9036, 10.4806] },
-  { markerOffset: -15, name: "Lima", coordinates: [-77.0428, -12.0464] }
+  { markerOffset: -15, name: "test", coordinates: [8,50] },
+  { markerOffset: 25, name: "test2", coordinates: [9,50] }
+  
 ];
 
 const MapChart = ({ setTooltipContent }) => {
